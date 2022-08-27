a624a7f23fc4830a958358cd62972b583b18ebb4
theduonghn <theduongctn@gmail.com>
2022-08-27 10:49:31 +0700

    Add field avatar to user. Remove entity image

--- book-online/src/main/java/vn/techmaster/bookonline/repository/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package vn.techmaster.bookonline.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import vn.techmaster.bookonline.entitiy.Image;
-
-public interface ImageRepository extends JpaRepository<Image, String> {
-}
