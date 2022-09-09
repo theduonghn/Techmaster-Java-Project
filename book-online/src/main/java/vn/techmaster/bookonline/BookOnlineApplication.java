ca8613058df1ed0a3fbb86e955efabe405d352d9
theduonghn <theduongctn@gmail.com>
2022-09-09 19:39:29 +0700

    Init data with test instead of application runner

--- book-online/src/main/java/vn/techmaster/bookonline/BookOnlineApplication.java
+++ book-online/src/main/java/vn/techmaster/bookonline/BookOnlineApplication.java
@@ -1,21 +1,11 @@
 package vn.techmaster.bookonline;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookOnlineApplication implements ApplicationRunner {
-	@Autowired
-	private MockDataInitializer mockDataInitializer;
-
+public class BookOnlineApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookOnlineApplication.class, args);
-	}
-
-	public void run(ApplicationArguments args) throws Exception {
-		mockDataInitializer.initData();
 	}
 }
---
+++ book-online/src/test/java/vn/techmaster/bookonline/repository/InitDataTest.java
@@ -0,0 +1,17 @@
+package vn.techmaster.bookonline.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import vn.techmaster.bookonline.MockDataInitializer;
+
+@SpringBootTest
+public class InitDataTest {
+    @Autowired
+    private MockDataInitializer mockDataInitializer;
+
+    @Test
+    void initData() {
+        mockDataInitializer.initData();
+    }
+}
